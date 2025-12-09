3b55ebf56fea356f91f27dbfe6f8ef5dca05a743
Alejandro Del Valle <alejandrodelvalle06@outlook.es>
2025-12-09 12:30:51 +0100

    Continuo con la practica 3

--- Tema_05/Practica_03/Files/detectar_codif.py
+++ Tema_05/Practica_03/Files/detectar_codif.py
@@ -2,8 +2,10 @@ import chardet
 import sys
 import os
 
-def detectar_codificacion(ruta):
+def detectar_codificacion(ruta) -> str:
     with open(ruta, "rb") as f:
         data = f.read()
     resultado = chardet.detect(data)
     return resultado["encoding"]
+
+#TOD: INSTALAR CHARDET
--- Tema_05/Practica_03/Practica03.py
+++ Tema_05/Practica_03/Practica03.py
@@ -2,8 +2,17 @@
 Práctica 03 de Python: Manejo de ficheros
 Autor: Alejandro del Valle Vallés
 """
+
+import os
+import Files.detectar_codif as detect_codf
+from pathlib import Path
+import json
+
 MENU_OPTIONS: tuple[str] = ("Conversión UTF-8 a Latin-1", "Conversión Latin-1 a UTF-8", 
                             "Conversión JSON a Longitud Fija.", "Conversión Longitud Fija a JSON")
+FILES_PATH: str = Path("Files").absolute()
+
+# QUESTION METHODS
 
 def ask_menu_option() -> int:
     """
@@ -39,9 +48,33 @@ def ask_user_want_continue() -> bool:
                 raise Exception
         except:
             print("La respuesta debe ser 'si' o 'no'.")
+    clean_console()
     return response == "si";
             
+def ask_file_path() -> str:
+    """
+    Ask the user to introduce a path. It can be absoluthe or variable path.
+    Returns:
+        str: absolute path 
+    """
+    response: str = ""
+    path: Path
+    while response == "":
+        try:
+            response = input("Introduce la ruta del fichero: ").strip()
+            clean_console()
+            if response == "":
+                raise Exception
+            
+            path = Path(response)
+            if not path.exists() or not path.is_file():
+                raise Exception
+            response = path.absolute()
+        except:
+            print("La ruta del fichero no puede estar vacía, tiene que existir y ser un fichero.")
+    return response
 
+# MENU METHODS
 
 def execute_menu_option(option: int):
     """
@@ -73,6 +106,29 @@ def show_menu():
         print(f"{i}. {option}")
         i += 1
     print("0. Salir")
+    
+def clean_console():
+    """
+    Clean the console.
+    """
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+# CONVERT METHDOS
+
+def convert_file(origin_encode: str, target_encode: str, origin_path: str, target_path: str) -> bool:
+    encoded: bool
+    
+    try:
+        with open(origin_path, 'r', encoding=origin_encode) as file_in:
+            content = file_in.read()
+
+        with open(target_path, 'w', encoding=target_encode, errors="ignore") as file_out:
+            file_out.write(content)
+        encoded = True
+    except:
+        encoded = False
+        
+    return encoded
 
 def convert_utf8_to_latin1():
     """
@@ -81,7 +137,14 @@ def convert_utf8_to_latin1():
     print("Antes de continuar, si el fichero contiene emojis u otros caracteres no sporotados por Latin-1 se eliminarán.")
     want_continue = ask_user_want_continue()
     if want_continue:
-        pass
+        file_path = ask_file_path()
+        name, ext = os.path.splitext(file_path)
+        output_filepath = f"{name}_latin1{ext}"
+        encoded = convert_file("utf-8", "latin-1", file_path, output_filepath)
+        if encoded:
+            print(f"Se ha convertido correctamente en {FILES_PATH}/{output_filepath}")
+        else:
+            print("No se ha podido convertir el fichero.")
     else:
         print("Acción cancelada.")
 
@@ -89,19 +152,46 @@ def convert_latin1_to_utf8():
     """
     Trasnform a file from Latin-1 to UTF-8.
     """
-    pass
+    file_path = ask_file_path()
+    name, ext = os.path.splitext(file_path)
+    output_filepath = f"{name}_utf8{ext}"
+    encoded = convert_file("latin-1", "utf-8", file_path, output_filepath)
+    if encoded:
+        print(f"Se ha convertido correctamente en {FILES_PATH}/{output_filepath}")
+    else:
+        print("No se ha podido convertir el fichero.")
 
 def convert_json_to_fixed_length():
     """
     Trasnform a JSON file to a fixed length txt file.
+    Number of Characters
+    Nombre: 10
+    Apellidos: 30
+    Edad: 3
+    Nombre usuario: 10
+    Telefono: 15
+    Contraseña: 20
+    Dirección: 80
     """
-    pass
+    file_path = ask_file_path()
+    if file_path[::-5] == ".json":
+        pass
+    else:
+        print("El fichero no es un fichero JSON.")
 
 def convert_fixed_length_to_json():
     """
     Trasnform a fixed length txt file to a JSON file.
     """
-    pass
+    file_path = ask_file_path()
+    if file_path[::-4] == ".txt":
+        with open(file_path, 'r', encoding=detect_codf.detectar_codificacion(file_path)) as file:
+            for line in file:
+                print(line)
+    else:
+        print("El fichero no es un fichero de longitud fija.")
+
+# MAIN
 
 def main():
     try:
@@ -109,6 +199,7 @@ def main():
         while option != 0:
             show_menu()
             option = ask_menu_option()
+            clean_console()
             execute_menu_option(option)
     except Exception as ex:
         print(f"Ha ocurrido un error inesperado durante la ejecución del programa: {ex}")
